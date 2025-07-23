b95eafb3e3325df3328107ad2fa3e5e44ebcaedc
colaola20 <soryo@farmingdale.edu>
2025-07-22 22:37:39 -0400

    added a link to comic details

--- src/Components/Comic.jsx
+++ src/Components/Comic.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Comic = ({id, title, image, printPrice, saleDate}) => {
     // Format the date nicely
@@ -13,14 +14,19 @@ const Comic = ({id, title, image, printPrice, saleDate}) => {
 
     return (
        <li className="main-list" key={id}>
-            <img className="comicImg" src={image} alt={title}/>
-            <div className="comic-content">
-                <div className="comic-title">{title}</div>
-                <div className="comic-details">
-                    <div className="comic-price"> Print price: ${printPrice}</div>
-                    <div className="comic-date"> Sale Date: {formatDate(saleDate)}</div>
+            <Link style={{color: "White" }}
+                to={`/comicDetails/${id}`}
+                key={id}
+            >
+                <img className="comicImg" src={image} alt={title}/>
+                <div className="comic-content">
+                    <div className="comic-title">{title}</div>
+                    <div className="comic-details">
+                        <div className="comic-price"> Print price: ${printPrice}</div>
+                        <div className="comic-date"> Sale Date: {formatDate(saleDate)}</div>
+                    </div>
                 </div>
-            </div>
+            </Link>
        </li>
     )
 }
